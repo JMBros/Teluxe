d5ee128d50d250e2777c2aa681f8dab3d13305ca
JMBros <justinmichaelbros@gmail.com>
2014-12-10 19:35:21 -0800

    Initial Commit

---
+++ Teluxe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Teluxe
+//
+//  Created by Justin Chiang on 12/10/14.
+//  Copyright (c) 2014 Justin Chiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Teluxe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Teluxe
+//
+//  Created by Justin Chiang on 12/10/14.
+//  Copyright (c) 2014 Justin Chiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
